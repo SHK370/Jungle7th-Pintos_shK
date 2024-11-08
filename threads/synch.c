de0971c688d5cc5fbf5feea71610b49e8316402f
susuggang <tn5323@gmail.com>
2024-11-08 15:54:39 +0900

    Update synch.c - sema up - add {}

--- threads/synch.c
+++ threads/synch.c
@@ -113,12 +113,14 @@ sema_up (struct semaphore *sema) {
 	ASSERT (sema != NULL);
 
 	old_level = intr_disable ();
-	if (!list_empty (&sema->waiters))
+	if (!list_empty (&sema->waiters)) {
 		
 		list_sort (&sema->waiters, thread_compare_priority, 0);
 
 		thread_unblock (list_entry (list_pop_front (&sema->waiters),
 					struct thread, elem));
+	}
+	
 	sema->value++;
 
 	thread_test_preemption ();
